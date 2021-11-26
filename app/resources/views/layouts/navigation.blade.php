f6c33645b0c64de0ad2fe97b5065b099816b3349
miyayamaM <59919826+miyayamaM@users.noreply.github.com>
2021-11-26 11:04:52 +0900

    認証系のブラウザテスト作成

--- app/resources/views/layouts/navigation.blade.php
+++ app/resources/views/layouts/navigation.blade.php
@@ -32,7 +32,7 @@
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out" dusk="myname_on_navigation">
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
@@ -50,7 +50,7 @@
 
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
-                                                this.closest('form').submit();">
+                                                this.closest('form').submit();" dusk="logout">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
@@ -92,7 +92,7 @@
 
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
-                                        this.closest('form').submit();">
+                                        this.closest('form').submit();" dusk="logout">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
--- app/tests/Browser/AuthorizationTest.php
+++ app/tests/Browser/AuthorizationTest.php
@@ -11,21 +11,53 @@ class AuthorizationTest extends DuskTestCase
 {
     use DatabaseMigrations;
 
-    /**
-     * A basic browser test example.
-     *
-     * @return void
-     */
-    public function testLogin()
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // テストケースごとにログアウトするためクッキーを消す
+        foreach (static::$browsers as $browser) {
+            $browser->driver->manage()->deleteAllCookies();
+        }
+    }
+
+    public function test_register_new_user()
+    {   
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/register')
+                    ->type('name', "test_user")
+                    ->type('email', "test@example.com")
+                    ->type('password', 'password')
+                    ->type('password_confirmation', 'password')
+                    ->press('登録')
+                    ->assertPathIs('/dashboard')
+                    ->assertSee('test_user');
+        });
+    }
+
+    public function test_login()
     {   
         $user = User::factory()->create();
         $this->browse(function (Browser $browser) use ($user) {
             $browser->visit('/login')
                     ->type('email', $user->email)
                     ->type('password', 'password')
-                    ->screenshot("test")
                     ->press('#login')
+                    ->assertPathIs('/dashboard')
                     ->assertSee('Dashboard');
+        });
+    }
+
+    public function test_logout()
+    {   
+        $user = User::factory()->create();
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs(User::find(1))
+                    ->visit('/dashboard')
+                    ->click('@myname_on_navigation')
+                    ->click('@logout')
+                    ->assertPathIs('/login')
+                    ->assertSee('パスワードを忘れた場合');
         });
     }
 }
