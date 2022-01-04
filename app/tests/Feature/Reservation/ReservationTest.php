13745fb9bdfcbb796adc05df41a19e978951b12e
miyayamaM <59919826+miyayamaM@users.noreply.github.com>
2022-01-04 14:57:24 +0900

    予約照会のfeatureテスト

---
+++ app/tests/Feature/Reservation/ReservationTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use App\Models\Item;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ReservationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $user;
+    protected $another_user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()
+            ->hasAttached(
+                Item::factory()->count(3),
+                [
+                    'start_date' => '2021-10-01',
+                    'end_date' => '2021-11-01',
+                ],
+                'reservations',
+            )
+            ->create();
+
+        $this->another_user = User::factory()
+            ->hasAttached(
+                Item::factory()->count(3),
+                [
+                    'start_date' => '2021-10-01',
+                    'end_date' => '2021-11-01',
+                ],
+                'reservations',
+            )
+            ->create();
+    }
+
+    public function test_自分が予約している物品一覧を見れる()
+    {
+        $response = $this->actingAs($this->user)
+                        ->get(route('user.reservations', ['id' => $this->user->id]));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_別のユーザーが予約している物品一覧を見れる()
+    {
+        $response = $this->actingAs($this->user)
+                        ->get(route('user.reservations', ['id' => $this->another_user->id]));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_存在しないユーザーの予約照会は404を返す()
+    {
+        $non_existent_user_id = User::all()->max('id') + 1;
+        $response = $this->actingAs($this->user)
+            ->get(route('user.reservations', ['id' => $non_existent_user_id]));
+
+        $response->assertStatus(404);
+    }
+}
